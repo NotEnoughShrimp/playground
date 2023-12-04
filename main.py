9060a4825570bd71fc8d42fd1b75f51f36216104
HowManyShrimp <kim_yonghui@outlook.com>
2023-12-04 11:50:44 -0500

    Update main.py
    added methods for getting the ability modifier and ability bonus.

--- main.py
+++ main.py
@@ -8,10 +8,58 @@ class Stat_Block:
         self.charisma = charisma
         self.level = level
         self.name = name
-    
+        
+        self.str_bonus = 0
+        self.dex_bonus = 0
+        self.con_bonus = 0
+        self.int_bonus = 0
+        self.wis_bonus = 0
+        self.char_bonus = 0
+        
+    def apply_modifier(self):
+        self.str_bonus = self.mod_value(self.strength)
+        self.dex_bonus = self.mod_value(self.dexterity)
+        self.con_bonus = self.mod_value(self.constitution)
+        self.int_bonus = self.mod_value(self.intelligence)
+        self.wis_bonus = self.mod_value(self.wisdom)
+        self.char_bonus = self.mod_value(self.charisma)
+        
+    def mod_value(self, stat_value):
+        if stat_value == 1:
+            return -5
+        elif 2 <= stat_value <= 3:
+            return -4
+        elif 4 <= stat_value <= 5:
+            return -3
+        elif 6 <= stat_value <= 7:
+            return -2
+        elif 8 <= stat_value <= 9:
+            return -1
+        elif 10 <= stat_value <= 11:
+            return 0
+        elif 12 <= stat_value <= 13:
+            return 1
+        elif 14 <= stat_value <= 15:
+            return 2
+        elif 16 <= stat_value <= 17:
+            return 3
+        elif 18 <= stat_value <= 19:
+            return 4
+        elif 20 <= stat_value <= 21:
+            return 5
+        elif 22 <= stat_value <= 23:
+            return 6
+        elif 24 <= stat_value <= 25:
+            return 7
+        elif 26 <= stat_value <= 27:
+            return 8
+        elif 28 <= stat_value <= 29:
+            return 9
+        elif stat_value == 30:
+            return 10
     def __str__(self) -> str:
         return f"Strength: {self.strength}\nDexterity: {self.dexterity}\nConstitution: {self.constitution}\nIntelligence: {self.intelligence}\nWisdom: {self.wisdom}\nCharisma: {self.charisma}"
-
+    
 class Player(Stat_Block):
     def __init__(self, strength=8, dexterity=8, constitution=8, intelligence=8, wisdom=8, charisma=8, level=None, name=None, experience=0):
         super().__init__(strength, dexterity, constitution,intelligence,wisdom,charisma,level,name)
