b409bbca9641e4c3be71fc4a054d81531e46fd4a
HowManyShrimp <kim_yonghui@outlook.com>
2023-12-11 14:59:53 -0500

    wip-gui
    just a wip, may be deleted later.

---
+++ testgui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("Playground")
+root.configure(background="blue")
+root.minsize(200,200)
+root.maxsize(500,500)
+root.geometry("300x300+50+50")
+root.mainloop()
